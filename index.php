756def5435003ab4ba73bc363398b679aba19175
maarten <19326293+afx42@users.noreply.github.com>
2019-10-14 09:50:14 +0200

    Update index.php
    added Rick's code

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
+echo gethostname();
+echo "<br>";
+\$password = "${password}";
+\$servername = "${mysqlip}";
+\$username = "apache";
+\$dbname = "employees";
 
-// Show all information, defaults to INFO_ALL
-phpinfo();
+\$conn = new mysqli(\$servername, \$username, \$password, \$dbname);
+if (\$conn->connect_error) {
+    die("Connection failed: " . \$conn->connect_error);
+}
 
+\$sql = "SELECT emp_no, first_name, last_name FROM employees WHERE emp_no <= 10050";
+\$result = \$conn->query(\$sql);
+
+if (\$result->num_rows > 0) {
+    while(\$row = \$result->fetch_assoc()) {
+        echo "Employee number: " . \$row["emp_no"]. " - Name: " . \$row["first_name"]. " " . \$row["last_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+\$conn->close();
 ?>
